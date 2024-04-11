ebea552f45f16f38c9014b4645be21972bb18e57
Sireen Gothadiya <er.sireen@gmail.com>
2024-04-11 23:12:06 +0530

    ALLOWED_HOST Update

--- collegeservices/settings.py
+++ collegeservices/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 
-if os.environ.get("DEBUG_VALUE") == "True":
-    ALLOWED_HOSTS = ["127.0.0.1"]
-else:
-    ALLOWED_HOSTS = ["collegeservices.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
