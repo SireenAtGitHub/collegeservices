add30bc7aaff87760449218a073788f400a70f88
Sireen Gothadiya <er.sireen@gmail.com>
2024-04-11 22:29:07 +0530

    DATABASE change

--- collegeservices/settings.py
+++ collegeservices/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG_VALUE"))
-
+print("**********************************")
+print(bool(os.getenv("DEBUG_VALUE")))
+print("**********************************")
 if bool(os.getenv("DEBUG_VALUE")):
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 else:
