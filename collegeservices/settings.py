a848c4ba1508986233a32014c05735e5ee370fe6
Sireen Gothadiya <er.sireen@gmail.com>
2024-04-11 22:41:41 +0530

    DEBUG set

--- collegeservices/settings.py
+++ collegeservices/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG_VALUE"))
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 print("******************************************")
 print(bool(os.getenv("DEBUG_VALUE")))
