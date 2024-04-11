9cc0fb2eb47bf9826814232a8815110238bf5318
Sireen Gothadiya <er.sireen@gmail.com>
2024-04-11 23:17:47 +0530

    Allow CORS

--- collegeservices/settings.py
+++ collegeservices/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders"
     "rest_framework",
     "api",
 ]
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "collegeservices.urls"
 
